221cefd6aab7b0d1f05a3dbc2ee34efc671f23e6
coharou <60243200+coharou@users.noreply.github.com>
2021-03-07 19:52:54 -0500

    Removed background image

--- TBQuestGame/Properties/Resources.Designer.cs
+++ TBQuestGame/Properties/Resources.Designer.cs
@@ -73,16 +73,6 @@ namespace TBQuestGame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap background {
-            get {
-                object obj = ResourceManager.GetObject("background", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap bottom_banner {
             get {
                 object obj = ResourceManager.GetObject("bottom_banner", resourceCulture);
